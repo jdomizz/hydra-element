a0f43af234807b57cd88f4ef2514d44ab4e26d8b
jdomizz <jdomidiaz@gmail.com>
2021-08-13 23:24:40 +0200

    added JSDoc

--- src/hydra-element.js
+++ src/hydra-element.js
@@ -4,6 +4,49 @@ import HydraSynth from 'hydra-synth';
  * Custom element with a global instance of `hydra-synth` embedded.
  */
 export class HydraElement extends HTMLElement {
+
+  /**
+   * Width of the canvas element to render to
+   * @attr
+   * @type {number}
+   */
+  width = window.innerWidth;
+
+  /**
+   * Height of the canvas element to render to
+   * @attr
+   * @type {number}
+   */
+  height = window.innerHeight;
+
+  /**
+   * Autodetect audio (ask for microphone)
+   * @attr
+   * @type {Boolean}
+   */
+  audio = false;
+
+  /**
+   * Number of source buffers to create initially
+   * @attr
+   * @type {number}
+   */
+  sources = 4;
+
+  /**
+   * Number of output buffers to use
+   * @attr
+   * @type {number}
+   */
+  outputs = 4;
+
+  /**
+   * Precision of the shaders
+   * @attr
+   * @type {"highp" | "mediump" | "lowp"}
+   */
+  precision = 'highp';
+
   static get observedAttributes() {
     return [
       'width',
@@ -18,7 +61,6 @@ export class HydraElement extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
-    this.initElement();
     this.initCanvas();
     this.initHydraSynth();
   }
@@ -50,21 +92,9 @@ export class HydraElement extends HTMLElement {
     }
   }
 
-  initElement() {
-    this.width = window.innerWidth;
-    this.height = window.innerHeight;
-    this.audio = false;
-    this.sources = 4;
-    this.outputs = 4;
-    this.precision = 'highp';
-    this.options = {
-      detectAudio: this.audio,
-      numSources: this.sources,
-      numOutputs: this.outputs,
-      precision: this.precision,
-    }
-  }
-
+  /**
+   * Initialize the canvas element to render to.
+   */
   initCanvas() {
     const canvas = document.createElement('canvas');
     canvas.width = this.width;
@@ -74,13 +104,26 @@ export class HydraElement extends HTMLElement {
     this.canvas = canvas;
   }
 
+  /**
+   * Initialize the `hydra-synth` engine.
+   */
   initHydraSynth() {
+    this.options = {
+      detectAudio: this.audio,
+      numSources: this.sources,
+      numOutputs: this.outputs,
+      precision: this.precision,
+    }
     this.hydra = new HydraSynth({
       canvas: this.canvas,
       ...this.options,
     });
   }
 
+  /**
+   * Update the `hydra-synth` engine with the new option value.
+   * @param {*} option Partial of the `hydra-synth` options
+   */
   updateHydraSynth(option) {
     this.hydra = new HydraSynth({
       canvas: this.canvas,
