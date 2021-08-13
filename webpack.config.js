0aa306b553990e455c6f7d50e407965977fe34b2
jdomizz <jdomidiaz@gmail.com>
2021-08-13 23:26:08 +0200

    added webpack copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,27 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = function (env, { mode }) {
   const production = mode === 'production';
   return {
     mode: production ? 'production' : 'development',
-    target: 'web',
     entry: './hydra-element.js',
     output: {
       filename: 'hydra-element.js',
       chunkFormat: 'module',
+      clean: true,
     },
     resolve: {
       extensions: ['.js'],
       modules: ['src', 'node_modules']
     },
+    plugins: production ? [
+      new CopyPlugin({
+        patterns: [
+          { from: "LICENSE" },
+          { from: "custom-elements.json" },
+        ],
+      })
+    ] : [],
     devServer: {
       port: 9000,
       historyApiFallback: true,
