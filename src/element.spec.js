fc8234e3bd78aeb9daf4ea5be2fe0af0580deefd
Jesus Dominguez <j.domi.diaz@gmail.com>
2023-12-12 22:46:08 +0100

    add unit tests

--- src/element.spec.js
+++ src/element.spec.js
@@ -1,4 +1,5 @@
 import { html, fixture, expect } from '@open-wc/testing'
+import { spy } from 'sinon'
 import { HydraElement } from './element'
 
 describe('<hydra-element>', () => {
@@ -7,13 +8,91 @@ describe('<hydra-element>', () => {
 
   it('should pass the a11y audit', async () => {
     const el = await fixture(html`<hydra-element></hydra-element>`)
-    expect(el)
-      .shadowDom
-      .to.be.accessible()
+    expect(el).shadowDom.to.be.accessible()
   })
 
-  it('should...', async () => {
-    // TODO
+  it('should initialize with default options', async () => {
+    const el = await fixture(html`<hydra-element></hydra-element>`)
+    expect(el.code).to.equal('')
+    expect(el.canvas).to.exist
+    expect(el.canvas.width).to.equal(window.innerWidth)
+    expect(el.canvas.height).to.equal(window.innerHeight)
+    expect(el._options.width).to.equal(window.innerWidth)
+    expect(el._options.height).to.equal(window.innerHeight)
+    expect(el._options.autoLoop).to.be.true
+    expect(el._options.makeGlobal).to.be.true
+    expect(el._options.detectAudio).to.be.false
+    expect(el._options.numSources).to.equal(4)
+    expect(el._options.numOutputs).to.equal(4)
+    expect(el._options.precision).to.be.null
+    expect(el.transforms).to.deep.equal([])
+    expect(el.pb).to.be.null
+  })
+
+  it('should update options when attributes change', async () => {
+    const el = await fixture(html`<hydra-element></hydra-element>`)
+    el.setAttribute('width', '500')
+    el.setAttribute('height', '300')
+    el.setAttribute('global', 'false')
+    el.setAttribute('audio', 'true')
+    el.setAttribute('sources', '2')
+    el.setAttribute('outputs', '1')
+    el.setAttribute('precision', 'lowp')
+    expect(el.canvas.width).to.equal(500)
+    expect(el.canvas.height).to.equal(300)
+    expect(el._options.width).to.equal(500)
+    expect(el._options.height).to.equal(300)
+    expect(el._options.makeGlobal).to.be.false
+    expect(el._options.detectAudio).to.be.true
+    expect(el._options.numSources).to.equal(2)
+    expect(el._options.numOutputs).to.equal(1)
+    expect(el._options.precision).to.equal('lowp')
+  })
+  
+  it('should get code from textContent', async () => {
+    const el = await fixture(html`<hydra-element>osc(10).out()</hydra-element>`)
+    expect(el.code).to.equal('osc(10).out()')
+  })
+
+  it('should set code property', async () => {
+    const el = await fixture(html`<hydra-element></hydra-element>`)
+    el.code = 'osc(10).out()'
+    expect(el.code).to.equal('osc(10).out()')
+  })
+
+  it('should set canvas property', async () => {
+    const el = await fixture(html`<hydra-element></hydra-element>`)
+    const canvas = document.createElement('canvas')
+    el.canvas = canvas
+    expect(el.canvas).to.equal(canvas)
+  })
+
+  it('should set transforms property', async () => {
+    const el = await fixture(html`<hydra-element></hydra-element>`)
+    const fn = {
+      name: 'yourNoise',
+      type: 'src',
+      inputs: [
+        { type: 'float', name: 'scale', default: 5 },
+        { type: 'float', name: 'offset', default: 0.5 }
+      ],
+      glsl: `return vec4(vec3(_noise(vec3(_st*scale, offset*time))), 0.5);`
+    }
+    el.transforms = [fn]
+    expect(el.transforms).to.deep.equal([fn])
+  })
+
+  it('should set pb property', async () => {
+    const el = await fixture(html`<hydra-element></hydra-element>`)
+    el.pb = undefined
+    expect(el.pb).to.equal(undefined)
+  })
+
+  it('should tick hydra when tick method is called', async () => {
+    const el = await fixture(html`<hydra-element loop="false"></hydra-element>`)
+    el._hydra = { tick: spy() }
+    el.tick(0.1)
+    expect(el._hydra.tick).to.have.been.calledOnceWith(0.1)
   })
 
 })
--- src/helper.spec.js
+++ src/helper.spec.js
@@ -1,24 +1,48 @@
+import { expect } from '@open-wc/testing'
+import { parseNumber, parseJSON, parseOption } from './helper'
 
 describe('parseNumber', () => {
-
-  it('should...', () => {
-    // TODO
+  it('should return the parsed value if it is within the min and max range', () => {
+    expect(parseNumber('10', 0, 5, 15)).to.equal(10)
   })
 
+  it('should return the default value if the parsed value is less than the min range', () => {
+    expect(parseNumber('3', 10, 5, 15)).to.equal(10)
+  })
+
+  it('should return the default value if the parsed value is greater than the max range', () => {
+    expect(parseNumber('20', 10, 5, 15)).to.equal(10)
+  })
+
+  it('should return the default value if the input is not a valid number', () => {
+    expect(parseNumber('abc', 10, 5, 15)).to.equal(10)
+  })
 })
 
 describe('parseJSON', () => {
-
-  it('should...', () => {
-    // TODO
+  it('should return the parsed value if the input is a valid JSON string', () => {
+    expect(parseJSON('{"foo": "bar"}', {})).to.deep.equal({ foo: 'bar' })
   })
 
+  it('should return the default value if the input is not a valid JSON string', () => {
+    expect(parseJSON('not a JSON string', { foo: 'bar' })).to.deep.equal({ foo: 'bar' })
+  })
+  
+  it('should return the default value if the input is an empty string', () => {
+    expect(parseJSON('', { foo: 'bar' })).to.deep.equal({ foo: 'bar' })
+  })
 })
 
 describe('parseOption', () => {
-
-  it('should...', () => {
-    // TODO
+  it('should return the value if it is included in the options', () => {
+    expect(parseOption('foo', 'default', ['foo', 'bar', 'baz'])).to.equal('foo')
   })
 
+  it('should return the default value if the value is not included in the options', () => {
+    expect(parseOption('qux', 'default', ['foo', 'bar', 'baz'])).to.equal('default')
+  })
+
+  it('should return the default value if the options array is empty', () => {
+    expect(parseOption('foo', 'default', [])).to.equal('default')
+  })
 })
