8f1528ad09dcc95d896111aa2a361004ec9429a5
Jesus Dominguez <j.domi.diaz@gmail.com>
2023-12-12 22:46:29 +0100

    add jsdoc

--- src/element.js
+++ src/element.js
@@ -1,6 +1,21 @@
 import Hydra from 'hydra-synth'
 import { parseJSON, parseNumber, parseOption } from './helper'
 
+/**
+ * Default options for Hydra Element.
+ * @typedef {Object} DEFAULT_OPTIONS
+ * @property {HTMLCanvasElement} canvas - The canvas element to render on.
+ * @property {number} width - The width of the canvas.
+ * @property {number} height - The height of the canvas.
+ * @property {boolean} autoLoop - Whether to automatically loop the animation.
+ * @property {boolean} makeGlobal - Whether to make the hydra instance global.
+ * @property {boolean} detectAudio - Whether to detect audio input.
+ * @property {number} numSources - The number of audio sources.
+ * @property {number} numOutputs - The number of audio outputs.
+ * @property {Array} extendTransforms - An array of custom transform functions.
+ * @property {number} precision - The precision of the rendering.
+ * @property {Object} pb - The instance of `rtc-patch-bay` for streaming.
+ */
 const DEFAULT_OPTIONS = {
   canvas: null,
   width: window.innerWidth,
@@ -15,8 +30,16 @@ const DEFAULT_OPTIONS = {
   pb: null
 }
 
+/**
+ * A custom element that renders Hydra sketches.
+ * @extends HTMLElement
+ */
 export class HydraElement extends HTMLElement {
 
+  /**
+   * An array of attribute names to observe on the custom element.
+   * @returns {string[]}
+   */
   static get observedAttributes() {
     return [
       'width',
@@ -29,6 +52,10 @@ export class HydraElement extends HTMLElement {
     ]
   }
 
+  /**
+   * Creates an instance of Element.
+   * @constructor
+   */
   constructor() {
     super()
     this._code = ''
@@ -36,10 +63,18 @@ export class HydraElement extends HTMLElement {
     this.attachShadow({ mode: 'open' })
   }
 
+  /**
+   * Returns the canvas element associated with this element.
+   * @returns {HTMLCanvasElement} The canvas element.
+   */
   get canvas() {
     return this._options.canvas;
   }
 
+  /**
+   * Setter for the canvas property.
+   * @param {HTMLCanvasElement} value - The canvas element to set.
+   */
   set canvas(value) {
     if (this._options.canvas) {
       this.shadowRoot?.querySelector('canvas')?.remove()
@@ -50,10 +85,18 @@ export class HydraElement extends HTMLElement {
     }
   }
 
+  /**
+   * Get the transforms of the element.
+   * @returns {Object} The extended transforms.
+   */
   get transforms() {
     return this._options.extendTransforms;
   }
 
+  /**
+   * Setter for the transforms property.
+   * @param {Array<Function>} value - An array of functions to extend the transforms.
+   */
   set transforms(value) {
     this._options.extendTransforms = value;
     if (this._hydra) {
@@ -61,10 +104,19 @@ export class HydraElement extends HTMLElement {
     }
   }
 
+  /**
+   * Gets the value of pb.
+   *
+   * @returns {Object} The value of pb.
+   */
   get pb() {
     return this._options.pb;
   }
 
+  /**
+   * Sets the value of pb option.
+   * @param {Object} value - The value to set for pb.
+   */
   set pb(value) {
     this._options.pb = value;
     if (this._hydra) {
@@ -72,10 +124,18 @@ export class HydraElement extends HTMLElement {
     }
   }
 
+  /**
+   * Get the code of the element.
+   * @returns {string} The code of the element.
+   */
   get code() {
     return this._code;
   }
 
+  /**
+   * Setter for the code property.
+   * @param {string} value - The code to be set.
+   */
   set code(value) {
     this._code = value;
     if (this._hydra) {
@@ -83,6 +143,12 @@ export class HydraElement extends HTMLElement {
     }
   }
 
+  /**
+   * Called when an observed attribute has been added, removed, updated, or replaced.
+   * @param {string} attrName - The name of the attribute that was changed.
+   * @param {string|null} oldValue - The previous value of the attribute, or null if it didn't exist before.
+   * @param {string|null} newValue - The new value of the attribute, or null if it was removed.
+   */
   attributeChangedCallback(attrName, oldValue, newValue) {
     if (newValue !== oldValue) {
       this._options = this._getNewOptions(attrName, newValue)
@@ -92,6 +158,12 @@ export class HydraElement extends HTMLElement {
     }
   }
 
+  /**
+   * Invoked each time the custom element is appended into a document-connected element.
+   * If the element has a code block in its textContent, it will be evaluated and rendered on the canvas.
+   * If the canvas or hydra instance are not initialized, they will be initialized.
+   * @returns {void}
+   */
   connectedCallback() {
     if (this._code === '' && this.textContent) {
       this._code = this.textContent
@@ -108,12 +180,20 @@ export class HydraElement extends HTMLElement {
     }
   }
 
+  /**
+   * Updates the element's state based on the elapsed time since the last tick.
+   * @param {number} dt - The elapsed time in seconds.
+   */
   tick(dt) {
     if (this._hydra) {
       this._hydra.tick(dt)
     }
   }
 
+  /**
+   * Initializes the canvas element for the element.
+   * @private
+   */
   _initCanvas() {
     this.shadowRoot?.querySelector('canvas')?.remove()
     this._options.canvas = document.createElement('canvas')
@@ -124,11 +204,19 @@ export class HydraElement extends HTMLElement {
     this.shadowRoot?.appendChild(this._options.canvas)
   }
 
+  /**
+   * Initializes the Hydra instance with the provided options and extends the transforms with the provided functions.
+   * @private
+   */
   _initHydra() {
     this._hydra = new Hydra({ ...this._options })
     this._options.extendTransforms.forEach(fn => this._hydra.synth.setFunction(fn))
   }
 
+  /**
+   * Evaluates the code in a new async function and executes it.
+   * @private
+   */
   _evalCode() {
     const code = `(async () => { ${this._code} })()`
     if (this._options.makeGlobal) {
@@ -138,6 +226,12 @@ export class HydraElement extends HTMLElement {
     }
   }
 
+  /**
+   * Returns a new options object with the updated value for the specified attribute.
+   * @param {string} attrName - The name of the attribute to update.
+   * @param {any} newValue - The new value for the attribute.
+   * @returns {Object} - A new options object with the updated attribute value.
+   */
   _getNewOptions(attrName, newValue) {
     switch (attrName) {
       case 'width': return { ...this._options, width: parseNumber(newValue, DEFAULT_OPTIONS.width, 0) }
--- src/helper.js
+++ src/helper.js
@@ -1,28 +1,61 @@
+/**
+ * Parses a string into a number and returns it. If the parsed value is not a number or is outside the specified range, returns the default value.
+ * @param {string} value - The string to be parsed into a number.
+ * @param {number} defaultValue - The default value to be returned if the parsed value is not a number or is outside the specified range.
+ * @param {number} min - The minimum value allowed for the parsed number.
+ * @param {number} max - The maximum value allowed for the parsed number.
+ * @returns {number} - The parsed number or the default value if the parsed value is not a number or is outside the specified range.
+ */
 export function parseNumber(value, defaultValue, min, max) {
   const parsedValue = parseInt(value, 10)
-  return isNaN(parsedValue, min, max) 
+  return isNaN(parsedValue, min, max)
     ? defaultValue
     : parsedValue
 }
-  
+
+/**
+ * Parses a JSON string and returns the resulting JavaScript value.
+ * @param {string} value - The JSON string to parse.
+ * @param {*} defaultValue - The value to return if the JSON string is empty or cannot be parsed.
+ * @returns {*} The resulting JavaScript value.
+ */
 export function parseJSON(value, defaultValue) {
-  return value
-    ? JSON.parse(value)
-    : defaultValue
-}
+  let result = defaultValue
   
+  try {
+    result = JSON.parse(value)
+  } catch (error) {
+    // console.log(error);
+  }
+
+  return result
+}
+
+/**
+ * Parses a value and returns it if it is included in the options array, otherwise returns the default value.
+ * @param {*} value - The value to parse.
+ * @param {*} defaultValue - The default value to return if the value is not included in the options array.
+ * @param {Array} options - The array of options to check against the value.
+ * @returns {*} - The parsed value or the default value.
+ */
 export function parseOption(value, defaultValue, options) {
   return options.includes(value)
     ? value
     : defaultValue
 }
-  
+
+/**
+ * Checks if a value is NaN or outside of a given range.
+ * @param {number} value - The value to check.
+ * @param {number} min - The minimum value of the range (inclusive).
+ * @param {number} [max] - The maximum value of the range (inclusive).
+ * @returns {boolean} - True if the value is NaN or outside of the range, false otherwise.
+ */
 function isNaN(value, min, max) {
-  return max 
-    ? Number.isNaN(value) 
-      || value < min 
-      || value > max
-    : Number.isNaN(value) 
-      || value < min 
+  return max
+    ? Number.isNaN(value)
+    || value < min
+    || value > max
+    : Number.isNaN(value)
+    || value < min
 }
-  
