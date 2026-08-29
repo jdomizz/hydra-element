e07efd91243581188435bd9460a96ecd90259459
Jesús Domínguez <j.domi.diaz@gmail.com>
2026-08-29 04:29:34 +0200

    chore: simplify workflow - remove husky, lint-staged, CI only on PRs

--- wtr.config.js
+++ wtr.config.js
@@ -1,6 +1,8 @@
 import { playwrightLauncher } from '@web/test-runner-playwright'
 import { vitePlugin } from '@remcovaes/web-test-runner-vite-plugin'
 
+const isCI = !!process.env.CI
+
 export default {
   files: 'src/**/*.spec.js',
   plugins: [vitePlugin()],
@@ -8,7 +10,7 @@ export default {
     playwrightLauncher({
       product: 'chromium',
       launchOptions: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        args: isCI ? ['--no-sandbox', '--disable-setuid-sandbox'] : [],
       },
     }),
   ],
