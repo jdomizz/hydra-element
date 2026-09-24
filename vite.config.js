2c3a500aaa040ca58b945c4b1225380af69fad8c
Jesús Domínguez Díaz <j.domi.diaz@gmail.com>
2026-09-24 23:07:44 +0200

    chore(tooling): switch to pnpm, vite and vitest

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  define: {
+    global: 'globalThis',
+  },
   build: {
     lib: {
-      entry: 'index.js',
+      entry: {
+        'hydra-element': 'index.js',
+        context: 'src/context.js',
+      },
       formats: ['es'],
-      fileName: 'hydra-element'
-    }
+      fileName: (format, entryName) => `${entryName}.js`,
+    },
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis'
-      }
-    }
-  }
 })
